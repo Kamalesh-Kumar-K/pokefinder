16cac6d4d6552d0e19634da0f267aa5734f1c0b9
Kamalesh Kumar <137206464+Kamalesh-Kumar-K@users.noreply.github.com>
2024-06-20 22:53:56 +0530

    Update Pokedata.jsx

--- src/components/Pokedata.jsx
+++ src/components/Pokedata.jsx
@@ -70,7 +70,7 @@ const Pokedata = () => {
       <div className="row justify-content-center" style={{ marginTop: "50px" }}>
         {loading ? ( // Show a loading message while data is being fetched
           <div className="text-center fs-1 text-primary">
-            <p>Loading...</p>
+            <p>Loading... /t please wait</p>
           </div>
         ) : filteredPoke.length > 0 ? (
           filteredPoke.map((pokemon, index) => (
